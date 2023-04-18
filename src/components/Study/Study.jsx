99cdbd5d57d1641455d9cd49e4d5ea329526cc7e
danifatsey86 <fatseym@gmail.com>
2023-04-18 13:25:37 +0300

    dani commit

--- src/components/Study/Study.jsx
+++ src/components/Study/Study.jsx
@@ -9,23 +9,24 @@ function Study() {
                 <button className='btn-study'>Study</button>
                 </h1>
             <p className='text-study'>
-            SEKARANG INI DI MOSKOW TERDAPAT LEBIH DARI 100 MAHASISWA INDONESIA YANG SEDANG MENEMPUH PENDIDIKAN S1, S2, S3 DAN SPESIALIS.
+            <h1>SEKARANG INI DI MOSKOW TERDAPAT LEBIH DARI 100 MAHASISWA INDONESIA YANG SEDANG MENEMPUH PENDIDIKAN S1, S2, S3 DAN SPESIALIS.</h1>
+<h1>Beberapa universitas di Moscow yang memiliki mahasiswa Indonesia antara lain:</h1>
+<h1>1.	Moscow State Institute of International Relations (MGIMO University)</h1>
+<h1>2.	Moscow State University</h1>
+<h1>3.	Higher School of Economics (HSE University)</h1>
+<h1>4.	Bauman Moscow State Technical University (BMSTU)</h1>
+<h1>5.	Moscow Institute of Physics and Technology (MIPT)</h1>
+<h1>6.	Moscow Power Engineering Institute (MPEI)</h1>
+<h1>7.	Plekhanov Russian University of Economics</h1>
+<h1>8.	Russian State University for the Humanities (RSUH)</h1>
+<h1>9.	Moscow State Institute of Radio Engineering, Electronics and Automation (MIREA)</h1>
+<h1>Namun, ini hanya beberapa contoh universitas yang memiliki mahasiswa Indonesia. Ada banyak universitas lain di Moscow yang juga memiliki mahasiswa Indonesia, terutama yang menawarkan program studi yang populer seperti kedokteran, teknik, dan ekonomi. Untuk mendapatkan informasi yang lebih lengkap, sebaiknya Anda mencari informasi langsung dari universitas yang Anda minati atau dari agen pendidikan yang terpercaya.  </h1>
+<h1>MAYORITAS MAHASISWA BERKULIAH DENGAN JALUR BEASISWA, BERIKUT DAFTAR BEASISWA YANG TERSEDIA </h1>
 
 <br/>
-BERIKUT UNIVERSITAS UNIVERSITAS DIMANA MAHASISWA MAHASISWA INDONESIA BELAJAR DI MOSKOW.
-<br/>
-<br/>
-A UNI <br/>
-B UNI <br/>
-C UNI <br/>
-D UNI <br/>
-E UNI <br/>
-<br/>
-MAYORITAS MAHASISWA BERKULIAH DENGAN JALUR BEASISWA, BERIKUT DAFTAR BEASISWA YANG TERSEDIA
-<br/>
-BEASISWA PEMERINTAH RUSIA <br/>
-BEASISWA HSE <br/>
-BEASISWA SKOLTECH <br/>
+<h1>BEASISWA PEMERINTAH RUSIA</h1>
+<h1>BEASISWA HSE</h1>
+<h1>BEASISWA SKOLTECH</h1>
             </p>
         
 
--- src/components/Tips/Tips.jsx
+++ src/components/Tips/Tips.jsx
@@ -8,10 +8,24 @@ export default function tips(){
                 <button className='btn-tips'>Tips</button>
                 </h1>
             <div className='text-tips'>
-            <h1>MOSKOW ADALAH KOTA TERBESAR DIRUSIA, TOTAL PENDUDUK MOSKOW SAAT INI DIPERKIRAKAN LEBIH DARI 1 ORANG. HIDUP DI KOTA BESAR TENTUNYA BUKAN HAL YANG MUDAH TERLEBIH LAGI KALAU BUKAN DI NEGARA SENDIRI. BERIKUT TIPS TIPS DARI PERMOS UNTUK TINGGAL DI MOSKOW SEBAGAI PELAJAR:</h1>
-<h1>1. MENGIKUTI KEGIATAN PERMOS</h1> 
-<h1>2. SELALU JAGA SILAHTURAHMI DENGAN TEMAN TEMAN LAIN YANG BERKULIAH DI MOSKOW</h1>
-<h1>DLL</h1>
+            <h1>Moskow adalah kota terbesar dirusia, total penduduk moskow saat ini diperkirakan lebih dari 1 orang. Hidup di kota besar tentunya bukan hal yang mudah terlebih lagi kalau bukan di negara sendiri. Berikut tips tips dari permos untuk tinggal di moskow sebagai pelajar:</h1>
+<h1>1. Mengikuti kegiatan permos</h1> 
+<h1>2. Selalu jaga silahturahmi dengan teman teman lain yang berkuliah di moskow</h1>
+
+<h1>3. Persiapkan diri dengan baik sebelum berangkat ke Moscow. Pastikan Anda sudah mempersiapkan segala kebutuhan administratif seperti visa, asuransi kesehatan, dan dokumen-dokumen lainnya. Pastikan juga Anda sudah mempelajari tentang budaya dan kebiasaan masyarakat di Russia.
+</h1>
+
+<h1>4. Belajar bahasa Rusia sebelum berangkat. Bahasa Rusia merupakan bahasa utama yang digunakan di Moscow, sehingga akan sangat membantu jika Anda bisa berbahasa Rusia. Selain itu, belajar bahasa Rusia juga dapat membantu Anda berinteraksi dengan masyarakat setempat dan menjalin hubungan yang lebih baik.</h1>
+<h1>5. Cari informasi tentang kampus dan lingkungan sekitarnya. Pastikan Anda sudah memahami tentang kampus tempat Anda akan kuliah, termasuk fasilitas yang tersedia, kurikulum, dan sistem pendidikan yang digunakan. Selain itu, cari juga informasi tentang lingkungan sekitar kampus, seperti tempat makan, transportasi, dan tempat-tempat wisata yang dapat Anda kunjungi.
+</h1>
+<h1>6. Jangan ragu untuk mencari teman dan bergabung dengan komunitas mahasiswa Indonesia di Moscow. Bergabung dengan komunitas mahasiswa Indonesia dapat membantu Anda merasa lebih nyaman dan terhubung dengan orang-orang yang memiliki pengalaman serupa. Selain itu, teman-teman baru juga dapat membantu Anda dalam mengatasi kesulitan yang mungkin Anda hadapi selama kuliah di Moscow.
+</h1>
+<h1>7. Jangan lupa untuk menjaga kesehatan dan keamanan. Pastikan Anda selalu menjaga kesehatan dan keamanan selama berada di Moscow. Jangan lupa untuk mengikuti aturan yang berlaku, seperti menjaga jarak sosial dan menggunakan masker, serta menghindari tempat-tempat yang dianggap berbahaya.
+</h1>
+<h1>8. Manfaatkan kesempatan untuk menjelajahi Moscow dan Rusia. Selain fokus pada kuliah, jangan lupa untuk menjelajahi Moscow dan Rusia. Ada banyak tempat wisata menarik yang dapat Anda kunjungi, seperti Istana Kremlin, Katedral St. Basil, dan Museum Hermitage. Selain itu, Anda juga dapat mempelajari budaya dan sejarah Rusia yang kaya dan unik.
+    </h1>
+    
+<h1>Dll</h1>
             </div>
         </div>
     );
